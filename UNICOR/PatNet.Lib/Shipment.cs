92d22bf095bcadbb85e94a37a751106658a19872
Jonathan <jmoral4@gmail.com>
2014-09-26 13:06:28 -0400

    DPI bugfix
    Fixed critical bug in image rendering via OOXML where DPI and EMU needed
    to be calculated in order to render correctly at all scan resolutions.

--- UNICOR/PatNet.Lib/Shipment.cs
+++ UNICOR/PatNet.Lib/Shipment.cs
@@ -17,6 +17,7 @@ using DocumentFormat.OpenXml.Wordprocessing;
 using A = DocumentFormat.OpenXml.Drawing;
 using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
 using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
+using System.Windows.Media.Imaging;
 //using Novacode;
 using System.Drawing;
 
@@ -368,20 +369,26 @@ namespace PatNet.Lib
             {
                 MainDocumentPart mainPart = wordprocessingDocument.MainDocumentPart;
 
-                
+               
 
-                using (FileStream stream = new FileStream(fileName, FileMode.Open))
-                {
-                    ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Tiff);
-                    imagePart.FeedData(stream); 
-                    AddImageToBody(wordprocessingDocument, mainPart.GetIdOfPart(imagePart));
-                }
+                      ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+
+                    using (FileStream stream = new FileStream(fileName, FileMode.Open))
+                    {
+                        imagePart.FeedData(stream);
+                    }
+
+                    AddImageToBody(wordprocessingDocument, mainPart.GetIdOfPart(imagePart), fileName);
+
+
+
+
 
                
             }
         }
 
-        private static void AddImageToBody(WordprocessingDocument wordDoc, string relationshipId)
+        private static void AddImageToBody2(WordprocessingDocument wordDoc, string relationshipId)
         {
             // Define the reference of the image.
             var element =
@@ -449,7 +456,91 @@ namespace PatNet.Lib
             wordDoc.MainDocumentPart.Document.Body.AppendChild(new Paragraph(new Run(element)));
         }
 
+        private static void AddImageToBody(WordprocessingDocument wordDoc, string relationshipId, string fileName)
+        {
 
+            var img = new BitmapImage(new Uri(fileName, UriKind.RelativeOrAbsolute));
+            var widthPx = img.PixelWidth;
+            var heightPx = img.PixelHeight;
+            var horzRezDpi = img.DpiX;
+            var vertRezDpi = img.DpiY;
+            const int emusPerInch = 914400;
+            const int emusPerCm = 360000;
+            var maxWidthCm = 16.51;
+            var widthEmus = (long)(widthPx / horzRezDpi * emusPerInch);
+            var heightEmus = (long)(heightPx / vertRezDpi * emusPerInch);
+            var maxWidthEmus = (long)(maxWidthCm * emusPerCm);
+            if (widthEmus > maxWidthEmus)
+            {
+                var ratio = (heightEmus * 1.0m) / widthEmus;
+                widthEmus = maxWidthEmus;
+                heightEmus = (long)(widthEmus * ratio);
+            }
+
+            // Define the reference of the image.
+            var element =
+                 new Drawing(
+                     new DW.Inline(
+                         new DW.Extent() { Cx = widthEmus, Cy = heightEmus },
+                         new DW.EffectExtent()
+                         {
+                             LeftEdge = 0L,
+                             TopEdge = 0L,
+                             RightEdge = 0L,
+                             BottomEdge = 0L
+                         },
+                         new DW.DocProperties()
+                         {
+                             Id = (UInt32Value)1U,
+                             Name = "Picture 1"
+                         },
+                         new DW.NonVisualGraphicFrameDrawingProperties(
+                             new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                         new A.Graphic(
+                             new A.GraphicData(
+                                 new PIC.Picture(
+                                     new PIC.NonVisualPictureProperties(
+                                         new PIC.NonVisualDrawingProperties()
+                                         {
+                                             Id = (UInt32Value)0U,
+                                             Name = "New Bitmap Image.jpg"
+                                         },
+                                         new PIC.NonVisualPictureDrawingProperties()),
+                                     new PIC.BlipFill(
+                                         new A.Blip(
+                                             new A.BlipExtensionList(
+                                                 new A.BlipExtension()
+                                                 {
+                                                     Uri =
+                                                       "{28A0092B-C50C-407E-A947-70E740481C1C}"
+                                                 })
+                                         )
+                                         {
+                                             Embed = relationshipId,
+                                             CompressionState = A.BlipCompressionValues.Print
+                                         },
+                                         new A.Stretch(
+                                             new A.FillRectangle())),
+                                     new PIC.ShapeProperties(
+                                         new A.Transform2D(
+                                             new A.Offset() { X = 0L, Y = 0L },
+                                             new A.Extents() { Cx = widthEmus, Cy = heightEmus }),
+                                         new A.PresetGeometry(
+                                             new A.AdjustValueList()
+                                         ) { Preset = A.ShapeTypeValues.Rectangle }))
+                             ) { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
+                     )
+                     {
+                         DistanceFromTop = (UInt32Value)0U,
+                         DistanceFromBottom = (UInt32Value)0U,
+                         DistanceFromLeft = (UInt32Value)0U,
+                         DistanceFromRight = (UInt32Value)0U,
+                         EditId = "50D07946"
+                     });
+
+            // Append the reference to body, the element should be in a Run.
+            wordDoc.MainDocumentPart.Document.Body.AppendChild(new Paragraph(new Run(element)));
+        }
 
 
 
